ef5162a47e54419fe73773cd2d8699ef0198ea6d
Gennaro Jacopo Russo <gennarojacopo@libero.it>
2022-03-28 21:35:21 +0200

    start routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 import Home from './Components/Views/Home/Home';
+import Test from './Components/Views/Test/Test';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/test">Test</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/region/:region">
+            
+          </Route>
+          <Route path="/test" element={<Test />}>
+          </Route>
+          <Route path="/" element={<Home />}>
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
